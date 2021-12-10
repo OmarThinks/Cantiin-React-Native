2dadd185ece21ca5b250ee574fb44a5130266c6d
Omar Magdy <omar.magdy.code@gmail.com>
2021-12-10 21:49:54 +0200

    Added a currentPage as a state

--- CantiinReactNative/application/intents/ProductsListIntent.js
+++ CantiinReactNative/application/intents/ProductsListIntent.js
@@ -1,45 +1,33 @@
-import React from 'react'
 import {SafeAreaView, Text, View, ScrollView} from 'react-native';
+import React,{useState, useEffect, useContext, useReducer} from 'react';
+
 const axios = require('axios');
 
 export default function ProductsList() {
-    axios.get('https://www.cantiin.com/api/products/').then(function (response) {
+  
+  const [currentPage,setCurrentPage]= useState(1);
+  
+  
+  
+  axios.get('https://www.cantiin.com/api/products/').then(function (response) {
         const data = response.data;
         console.log(data);
       });
+  
+  
    return (
         <SafeAreaView>
         <View style={{
-          backgroundColor: 'blue',
-
-
-          height:"100%",width:"100%", padding:0
-          
-          
-          
-
-          }}>
+          backgroundColor: 'blue', height:"100%", width:"100%", padding:0}}>
           <View style={{
             backgroundColor: 'red', 
-            height:"90%", width:"100%"
-            
-            
-
-            }}>
-            <ScrollView style={{backgroundColor:"yellow", 
-            
-            
-            
-            
-            
-            
-            
-            }}>
+            height:"90%", width:"100%"}}>
+            <ScrollView style={{backgroundColor:"yellow",}}>
               <Text>Hi!</Text>
-
             </ScrollView>
           </View>
-          <View style={{backgroundColor: 'magenta', height:"10%", width:"100%", padding:0, margin:0}}>
+          <View style={{
+              backgroundColor: 'magenta', height:"10%", width:"100%", padding:0,      margin:0}}>
             <Text>Wassup</Text>
           </View>
         </View>
