993d0f4b65a9fba6bba1513141044d601c3eb7bd
Omar Magdy <omar.magdy.code@gmail.com>
2021-12-10 22:27:08 +0200

    rendered list of products

--- CantiinReactNative/application/intents/ProductsListIntent.js
+++ CantiinReactNative/application/intents/ProductsListIntent.js
@@ -1,10 +1,15 @@
-import {SafeAreaView, Text, View, ScrollView} from 'react-native';
-import React,{useState, useEffect, useContext, useReducer} from 'react';
+import {SafeAreaView, Text, View, ScrollView, FlatList} from 'react-native';
+import React,{useState, useEffect, useContext, useReducer, Fragment} from 'react';
 import styles from '../styles';
+import { ActivityIndicator, Colors } from 'react-native-paper';
 
 const axios = require('axios');
 
 
+
+const renderItem = ({ item }) => (
+  <Text >{item.id} </Text>
+);
 
 
 export default function ProductsList() {
@@ -18,15 +23,32 @@ export default function ProductsList() {
   }, [currentPage]);
 
   
+  let loading = (()=>{
+    if (response.length==0) {
+      return <ActivityIndicator animating={loading} color={Colors.red800} />;
+    }
+    return <Fragment/>
+  })();
+  console.log(loading);  
   
   console.log(response);
-  
+
    return (
         <SafeAreaView>
         <View style={{ backgroundColor: 'blue', ...styles.mainContainer }}>
           <View style={{ backgroundColor: 'red', ...styles.mainContent }}>
             <ScrollView style={{backgroundColor:"yellow",}}>
-              <Text>Hi!</Text>
+            {loading}
+            <SafeAreaView style={{flex: 1}}>
+              <FlatList
+                data={response.results}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+              />  
+            
+            </SafeAreaView> 
+
+
             </ScrollView>
           </View>
           <View style={{ backgroundColor: 'magenta', ...styles.mainFootBar }}>
