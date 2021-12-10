283b5e4db229f1f5a6567712e41bac9b41ada897
Omar Magdy <omar.magdy.code@gmail.com>
2021-12-10 17:19:53 +0200

    separated components into views

--- CantiinReactNative/App.js
+++ CantiinReactNative/App.js
@@ -7,26 +7,26 @@
  */
 
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 const axios = require('axios');
 
 const App = () => {
-  axios.get('https://www.cantiin.com/api/products/')
-  .then(function (response) {
+  axios.get('https://www.cantiin.com/api/products/').then(function (response) {
     const data = response.data;
     console.log(data);
   });
-  
   return (
-    <SafeAreaView >
-      <View style={{width:"100%", height:"100%", backgroundColor:"blue"}}>
-      
-        <Text style={{flexGrow:1, backgroundColor:"red", padding:30}}>Hi!</Text>
-        <Text style={{backgroundColor:"magenta", padding:20}}>Wassup</Text>
+    <SafeAreaView>
+      <View style={{width: '100%', height: '100%', backgroundColor: 'blue'}}>
+        <View style={{flexGrow: 1, backgroundColor: 'red', padding: 30}}>
+          <Text>Hi!</Text>
+        </View>
+        <View style={{backgroundColor: 'magenta', padding: 20}}>
+          <Text>Wassup</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
 };
-
 
 export default App;
