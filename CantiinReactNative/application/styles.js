647661f7c18c1c740206aa8531ecc0d815a09975
Omar Magdy <omar.magdy.code@gmail.com>
2021-12-11 22:13:08 +0200

    Better styling for bottom bar

--- CantiinReactNative/application/styles.js
+++ CantiinReactNative/application/styles.js
@@ -6,20 +6,22 @@ const mainContainer = {height: '100%', width: '100%'};
 const mainFootBar = {
   height: '10%',
   width: '100%',
-  backgroundColor: Colors.grey700,
+  backgroundColor: Colors.grey400,
 };
 const mainContent = {height: '90%', width: '100%'};
 const mainDivider = {height: 3};
-const footerButtonView = {width: '50%', height: '100%', padding: 7};
+const footerButtonView = {width: '50%', height: '100%', padding: 7 };
 const footerButton = {
   width: '100%',
   height: '100%',
   borderRadius: 10,
   backgroundColor: Colors.green200,
+  borderWidth: 1.5,
 };
 const footerButtonDisabled = {
   ...footerButton,
   backgroundColor: Colors.transparent,
+  borderWidth: 0,
 };
 const footerButtonStyle = disabled =>
   disabled ? {...footerButtonDisabled} : {...footerButton};
