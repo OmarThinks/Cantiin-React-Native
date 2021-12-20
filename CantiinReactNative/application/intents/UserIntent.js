cde65bd69a50f7eaacf39f8ee328a60f26495fa1
Omar Magdy <omar.magdy.code@gmail.com>
2021-12-20 23:21:37 +0200

    Created Intent UserIntent

---
+++ CantiinReactNative/application/intents/UserIntent.js
@@ -0,0 +1,10 @@
+import React, {Component, Fragment} from 'react';
+import {Text} from 'react-native-paper';
+
+export default function UserIntent() {
+  return (
+    <Fragment>
+      <Text>Heelo User</Text>
+    </Fragment>
+  );
+}
