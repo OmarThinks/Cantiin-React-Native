2c8d13f7333a0cce63390698ba4670b75a2ee2f5
Omar Magdy <omar.magdy.code@gmail.com>
2021-12-31 21:36:57 +0200

    Managed to make a custom component as a header

--- CantiinReactNative/application/App.js
+++ CantiinReactNative/application/App.js
@@ -30,12 +30,67 @@ import {
 import {View} from 'react-native';
 import {Colors} from 'react-native-paper';
 import {IconButton} from 'react-native-paper';
+import {getHeaderTitle} from '@react-navigation/elements';
 
 import {Appbar} from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
 const theme = {...DefaultTheme};
+
+/*
+const App = () => {
+  const MORE_ICON = 'dots-horizontal';
+  const MyComponent = () => (
+    <Appbar.Header>
+      <Appbar.Content title="Title" subtitle={'Subtitle'} />
+      <Appbar.Action icon="magnify" onPress={() => {}} />
+      <Appbar.Action icon={MORE_ICON} onPress={() => {}} />
+    </Appbar.Header>
+  );
+  return (
+    <NavigationContainer>
+      <PaperProvider theme={theme}>
+        <Stack.Navigator initialRouteName="ProductsList">
+          <Stack.Screen
+            name="ProductsList"
+            component={ProductsListIntent}
+            options={{
+              title: 'Products List',
+              headerStyle: {
+                backgroundColor: Colors.black,
+              },
+              headerTintColor: Colors.white,
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+              //headerShown: false,
+              headerRight: () => {
+                return (
+                  <View>
+                    <IconButton
+                      size={20}
+                      icon="account"
+                      style={{backgroundColor: 'white'}}
+                      onPress={() => {
+                        //console.log("Navigation", navigation);
+                        //navigation.navigate('UserIntent');
+                      }}
+                    />
+                  </View>
+                );
+              },
+            }}
+          />
+        </Stack.Navigator>
+      </PaperProvider>
+    </NavigationContainer>
+  );
+};
+
+
+
+*/
 
 const App = () => {
   const MORE_ICON = 'dots-horizontal';
@@ -62,6 +117,22 @@ const App = () => {
               headerTitleStyle: {
                 fontWeight: 'bold',
               },
+              header: ({navigation, route, options, back}) => {
+                const title = getHeaderTitle(options, route.name);
+
+                return (
+                  <MyComponent
+                    title={title}
+                    leftButton={
+                      back ? (
+                        <MyBackButton onPress={navigation.goBack} />
+                      ) : undefined
+                    }
+                    style={options.headerStyle}
+                  />
+                );
+              },
+
               //headerShown: false,
               headerRight: () => {
                 return (
