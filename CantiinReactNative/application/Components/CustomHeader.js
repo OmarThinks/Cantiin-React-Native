832f53fb65065c3682a6e6323ab798669dde6241
Omar Magdy <omar.magdy.code@gmail.com>
2021-12-31 23:57:54 +0200

    onPress function added to header

--- CantiinReactNative/application/Components/CustomHeader.js
+++ CantiinReactNative/application/Components/CustomHeader.js
@@ -6,17 +6,11 @@ const CustomHeader = props => {
   const subtitle = props.subtitle ? props.subtitle : undefined;
   const navigation = props.navigation ? props.navigation : undefined;
   const headerStyle = {backgroundColor: Colors.black};
-
+  const onPress = props.onPress ? props.onPress : () => {};
   return (
     <Appbar.Header style={headerStyle}>
       <Appbar.Content title={title} subtitle={subtitle} />
-      <Appbar.Action
-        icon="account"
-        onPress={() => {
-          console.log(navigation);
-          navigation.navigate('ProductsList1');
-        }}
-      />
+      <Appbar.Action icon="account" onPress={onPress} />
     </Appbar.Header>
   );
 };
