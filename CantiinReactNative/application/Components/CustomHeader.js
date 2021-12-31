bee5fa616f3af55e475fcff8a99ef2900cf39371
Omar Magdy <omar.magdy.code@gmail.com>
2021-12-31 23:46:53 +0200

    Refractor CustomHeader

--- CantiinReactNative/application/Components/CustomHeader.js
+++ CantiinReactNative/application/Components/CustomHeader.js
@@ -5,9 +5,11 @@ const CustomHeader = props => {
   const title = props.title;
   const subtitle = props.subtitle ? props.subtitle : undefined;
   const navigation = props.navigation ? props.navigation : undefined;
+  const headerStyle = {backgroundColor: Colors.black};
+
   return (
-    <Appbar.Header style={{backgroundColor: Colors.black}}>
-      <Appbar.Content title={props.title} subtitle={subtitle} />
+    <Appbar.Header style={headerStyle}>
+      <Appbar.Content title={title} subtitle={subtitle} />
       <Appbar.Action
         icon="account"
         onPress={() => {
