84c7c15e8d67b9da3cacd03f23b44a78662717f3
Omar Magdy <omar.magdy.code@gmail.com>
2022-01-01 22:25:53 +0200

    Username and email as state

--- CantiinReactNative/application/intents/UserIntent.js
+++ CantiinReactNative/application/intents/UserIntent.js
@@ -1,41 +1,44 @@
-import React, {Fragment} from 'react';
+import React, {useState} from 'react';
 import {Colors, TouchableRipple, Text, View, Button} from 'react-native-paper';
 import {SafeAreaView, FlatList, StyleSheet} from 'react-native';
 import CustomInputField from '../Components/CustomInputField';
 export default function UserIntent() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
   return (
     <SafeAreaView style={{backgroundColor: Colors.grey400, height: '100%'}}>
       <CustomInputField
-        label="Email"
-        //value={""}
-        //onChangeText={text => setText(text)}
+        label="Username or Email"
+        value={username}
+        onChangeText={text => setUsername(text)}
       />
       <CustomInputField
         label="Password"
-        //value={""}
-        //onChangeText={text => setText(text)}
+        value={password}
+        onChangeText={text => setPassword(text)}
         secureTextEntry={true}
       />
 
-        <Button
-          onPress={() => {
-            //setLoading(true);
-            //loadPage();
-          }}
-          style={{
-            backgroundColor: Colors.green300,
-            width: '50%',
-            alignSelf: 'center',
-            fontSize: 30,
-            margin:25
-          }}
-          labelStyle={{fontSize: 20}}
-          //disabled={loading}
-          //loading={loading}
-          compact={false}
-          contentStyle={{padding: 10}}>
-          Log In
-        </Button>
+      <Button
+        onPress={() => {
+          //setLoading(true);
+          //loadPage();
+        }}
+        style={{
+          backgroundColor: Colors.green300,
+          width: '50%',
+          alignSelf: 'center',
+          fontSize: 30,
+          margin: 25,
+        }}
+        labelStyle={{fontSize: 20}}
+        //disabled={loading}
+        //loading={loading}
+        compact={false}
+        contentStyle={{padding: 10}}>
+        Log In
+      </Button>
     </SafeAreaView>
   );
 }
