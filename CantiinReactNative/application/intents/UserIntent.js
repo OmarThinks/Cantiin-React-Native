e2e68e9d0566d39e4f63362c32d9c06b86bb5053
Omar Magdy <omar.magdy.code@gmail.com>
2022-01-01 18:55:34 +0200

    CustomInputField.js created

--- CantiinReactNative/application/intents/UserIntent.js
+++ CantiinReactNative/application/intents/UserIntent.js
@@ -1,23 +1,30 @@
 import React, {Fragment} from 'react';
-import {Text, TextInput} from 'react-native-paper';
+import {TextInput, Colors} from 'react-native-paper';
+import {SafeAreaView, View, FlatList, StyleSheet} from 'react-native';
 
 export default function UserIntent() {
   return (
-    <Fragment>
-      <Text>Heelo User</Text>
+    <SafeAreaView style={{backgroundColor:Colors.grey400, height:"100%"}}>
       <TextInput
-      label="Email"
-      //value={""}
-      //onChangeText={text => setText(text)}
-      mode="outlined"
-    />
+        label="Email"
+        //value={""}
+        //onChangeText={text => setText(text)}
+        mode="outlined"
+        outlineColor={Colors.green300}
+        activeOutlineColor={Colors.green700}
+        style={{backgroundColor:Colors.grey100}}
+        placeholderTextColor={Colors.grey700}
+        style={{margin:10}}
+      />
       <TextInput
-      label="Password"
-      //value={""}
-      //onChangeText={text => setText(text)}
-      mode="outlined"
-      secureTextEntry={true}
-    />
-    </Fragment>
+        label="Password"
+        //value={""}
+        //onChangeText={text => setText(text)}
+        mode="outlined"
+        outlineColor={Colors.green300}
+        activeOutlineColor={Colors.green700}
+        secureTextEntry={true}
+      />
+    </SafeAreaView>
   );
 }
