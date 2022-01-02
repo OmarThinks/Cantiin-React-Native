e41c7efb4b630cd1f2f1014b08a3016978f04c16
Omar Magdy <omar.magdy.code@gmail.com>
2022-01-02 18:26:36 +0200

    Sending request to server

--- CantiinReactNative/application/intents/UserIntent.js
+++ CantiinReactNative/application/intents/UserIntent.js
@@ -6,6 +6,24 @@ const axios = require('axios');
 
 ('https://cantiin.com/api/auth/custom/login/');
 
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response; // parses JSON response into native JavaScript objects
+}
+
 export default function UserIntent() {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
@@ -42,17 +60,26 @@ export default function UserIntent() {
       <Button
         onPress={() => {
           setLoading(true);
-          axios({
-            method: 'post',
-            url: 'https://cantiin.com/api/auth/custom/login/',
-            timeout: 1000 * 5, // Wait for n seconds
+          setLoginFailedText("");
+          console.log({username,password});
+          postData('https://cantiin.com/api/auth/custom/login/', {
+            username,
+            password,
           })
-            .then(function (responseOfRequest) {
-              setLoading(false);
-              console.log(responseOfRequest);
+            .then(data => {
+              console.log(data); // JSON data parsed by `data.json()` call
+              console.log(data.status);
+              if (data.status !== 200) {
+                setLoginFailedText('Wrong Username or Password');
+              }
+              data.json().then(jsonData=>console.log(jsonData));
             })
-            .catch(err => {
-              console.log(err);
+            .catch(() => {
+              setLoginFailedText(
+                'Something went wrong, Try again later, maybe you are not connected to the internet',
+              );
+            })
+            .finally(() => {
               setLoading(false);
             });
         }}
