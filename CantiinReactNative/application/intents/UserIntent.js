db04676bb482543b36443999a704d457a4050b73
Omar Magdy <omar.magdy.code@gmail.com>
2022-01-02 22:53:21 +0200

    Installed AsyncStorage

--- CantiinReactNative/application/intents/UserIntent.js
+++ CantiinReactNative/application/intents/UserIntent.js
@@ -20,30 +20,32 @@ export default function UserIntent() {
   const handleLoginPress = () => {
     setLoading(true);
     setLoginFailedText('');
-    console.log({username, password});
+    //console.log({username, password});
     sendData('POST', 'https://cantiin.com/api/auth/custom/login/', {
       username,
       password,
     })
       .then(data => {
-        console.log(data); // JSON data parsed by `data.json()` call
-        console.log(data.status);
+        //console.log(data); // JSON data parsed by `data.json()` call
+        //console.log(data.status);
         if (data.status !== 200) {
           setLoginFailedText('Wrong Username or Password');
         } else {
           const cookieData = {};
-          console.log(data.headers.map['set-cookie']);
+          //console.log(data.headers.map['set-cookie']);
           data.headers.map['set-cookie'].split(';').map(stringInput => {
             const singleCookieData = stringInput.split('=');
             cookieData[singleCookieData[0].trim()] = singleCookieData[1];
           });
-          console.log(cookieData);
+          //console.log(cookieData);
           console.log(cookieData['Secure, sessionid']);
         }
-        data.json().then(jsonData => console.log(jsonData));
+        data.json().then(jsonData => {
+          //console.log(jsonData);
+        });
       })
       .catch(() => {
-        console.log('failed');
+        //console.log('failed');
         setLoginFailedText(
           'Something went wrong, Try again later, maybe you are not connected to the internet',
         );
