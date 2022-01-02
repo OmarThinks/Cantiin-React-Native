8eb62487f576a4b1cc64afbe0a2d0cf7c085c806
Omar Magdy <omar.magdy.code@gmail.com>
2022-01-02 23:14:47 +0200

    AccountContext:Created

---
+++ CantiinReactNative/application/contexts/AccountContext.js
@@ -0,0 +1,50 @@
+import React from 'react-router-dom';
+import {createContext, Component} from 'react';
+
+export const AccountContext = createContext();
+
+/*
+state
+
+setAccountToken
+refreshAccountData
+*/
+
+class AccountContextProvider extends Component {
+  state = {
+    userId: null,
+    userData: null,
+    token: null,
+  };
+
+  setAccountToken = () => {
+    this.setState({
+      user: null,
+      userData: null,
+      userId: null,
+    });
+  };
+
+  refreshAccountData = () => {
+    this.setState({
+      user: null,
+      userData: null,
+      userId: null,
+    });
+  };
+
+  render() {
+    const parsedContext = {
+      ...this.state,
+      setAccountToken: this.setAccountToken,
+      refreshAccountData: this.refreshAccountData,
+    };
+    return (
+      <AccountContext.Provider value={parsedContext}>
+        {this.props.children}
+      </AccountContext.Provider>
+    );
+  }
+}
+
+export default AccountContextProvider;
