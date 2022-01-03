62b3d8cacb3f5855725a1582040f8a2fc3c5afe1
Omar Magdy <omar.magdy.code@gmail.com>
2022-01-03 22:16:34 +0200

    accountStorage user data

--- CantiinReactNative/application/helpers/Storage/accountStorage.js
+++ CantiinReactNative/application/helpers/Storage/accountStorage.js
@@ -1,1 +1,26 @@
+import React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const getData = async storage_Key => {
+  try {
+    return await AsyncStorage.getItem('@storage_Key');
+  } catch (e) {
+    return null;
+  }
+};
+const getDataJSON = async storage_Key => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('@storage_Key');
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    return null;
+  }
+};
+
+const getToken = () => {
+  return getData('token');
+};
+
+const getUserData = () => {
+  return getDataJSON('userData');
+};
