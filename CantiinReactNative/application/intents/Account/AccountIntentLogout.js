3d8fdd3c691083b1a22f26190ec803d2a8b2fb9f
Omar Magdy <omar.magdy.code@gmail.com>
2022-01-08 19:49:44 +0200

    Added Loading feature in logout

--- CantiinReactNative/application/intents/Account/AccountIntentLogout.js
+++ CantiinReactNative/application/intents/Account/AccountIntentLogout.js
@@ -1,39 +1,32 @@
-import React, {useState, Fragment, useContext, useEffect} from 'react';
-import {
-  Text,
-  Button,
-  ActivityIndicator,
-  Colors,
-  Paragraph,
-} from 'react-native-paper';
+import React, {useState, Fragment, useContext} from 'react';
+import {Text, Button} from 'react-native-paper';
 import {SafeAreaView} from 'react-native';
-import CustomInputField from '../../Components/CustomInputField';
-import sendData from '../../helpers/sendData';
 import styles from '../../styles';
 import {AccountContext} from '../../contexts/AccountContext';
 
 export default function AccountIntentLogout() {
+  const [loading, setLoading] = useState(false);
   const {accountData} = useContext(AccountContext);
+  const displayedUsernameText = `Your Username: ${accountData.userData.username}`;
   let additionalContent = <Fragment />;
   if (accountData.userData != null) {
     console.log('accountData is', accountData.userData.id);
     additionalContent = (
       <Fragment>
-        <Text style={{...styles.userData}}>Your Username: {accountData.userData.username}</Text>
+        <Text style={{...styles.userData}}>{displayedUsernameText}</Text>
       </Fragment>
     );
   }
 
   return (
-    <SafeAreaView
-      style={{...styles.mainAccountContainer, justifyContent: 'center'}}>
+    <SafeAreaView style={{...styles.mainContainerCenter}}>
       {additionalContent}
       <Button
         //onPress={handleLoginPress}
         style={{...styles.requestButton}}
         labelStyle={styles.requestButtonLabel}
-        //disabled={loading}
-        //loading={loading}
+        disabled={loading}
+        loading={loading}
         compact={false}
         contentStyle={styles.requestButtonContent}>
         Log Out
