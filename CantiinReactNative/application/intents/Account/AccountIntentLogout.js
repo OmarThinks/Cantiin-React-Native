c32525fcdd846bb187feb50f4532628c9321a035
Omar Magdy <omar.magdy.code@gmail.com>
2022-01-08 17:41:08 +0200

    Displaying Account Data

--- CantiinReactNative/application/intents/Account/AccountIntentLogout.js
+++ CantiinReactNative/application/intents/Account/AccountIntentLogout.js
@@ -1,5 +1,11 @@
 import React, {useState, Fragment, useContext, useEffect} from 'react';
-import {Text, Button, ActivityIndicator, Colors} from 'react-native-paper';
+import {
+  Text,
+  Button,
+  ActivityIndicator,
+  Colors,
+  Paragraph,
+} from 'react-native-paper';
 import {SafeAreaView} from 'react-native';
 import CustomInputField from '../../Components/CustomInputField';
 import sendData from '../../helpers/sendData';
@@ -7,9 +13,22 @@ import styles from '../../styles';
 import {AccountContext} from '../../contexts/AccountContext';
 
 export default function AccountIntentLogout() {
+  const {accountData} = useContext(AccountContext);
+  let additionalContent = <Fragment />;
+  if (accountData.userData != null) {
+    console.log('accountData is', accountData.userData.id);
+    additionalContent = (
+      <Fragment>
+        <Paragraph> {accountData.userData.id}</Paragraph>
+        <Paragraph>{accountData.userData.username}</Paragraph>
+      </Fragment>
+    );
+  }
+
   return (
     <SafeAreaView
       style={{...styles.mainAccountContainer, justifyContent: 'center'}}>
+      {additionalContent}
       <Button
         //onPress={handleLoginPress}
         style={{...styles.requestButton}}
